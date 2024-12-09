398ccc9d904e8e52becba190e6a9e268314b6387
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2024-12-08 22:11:27 -0300

    feat: creating login

---
+++ modulo10/template-materialui/src/pages/auth/Login.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { useColorScheme } from "@mui/joy/styles";
+import Sheet from "@mui/joy/Sheet";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Typography from "@mui/joy/Typography";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
+
+export default function LoginFinal() {
+  return (
+    <main>
+      <Sheet
+        sx={{
+          width: 300,
+          mx: "auto",
+          my: 4,
+          py: 3,
+          px: 2,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          borderRadius: "sm",
+          boxShadow: "md",
+        }}
+        variant="outlined"
+      >
+        <div>
+          <Typography level="h4" component="h1">
+            <b>Welcome!</b>
+          </Typography>
+          <Typography level="body-sm">Sign in to continue.</Typography>
+        </div>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input name="email" type="email" placeholder="johndoe@email.com" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input name="password" type="password" placeholder="password" />
+        </FormControl>
+        <Button sx={{ mt: 1 }}>Log in</Button>
+      </Sheet>
+    </main>
+  );
+}
